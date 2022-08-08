cd8642718e0d2a31721e22216efe92387b1e89e4
Omachi John <omachi.john@.com>
2022-08-08 14:00:30 +0100

    Add book class to a module, Import book class from book module

---
+++ modules/luxonTime.js
@@ -0,0 +1,16 @@
+export class Time {
+  static displayTime() {
+    const dt = luxon.DateTime.now()
+      .setLocale(navigator.language)
+      .toLocaleString({
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: 'false',
+      });
+
+    document.querySelector('time').innerHTML = dt;
+  }
+}
